c92fd6d48eba674e7444c84247d1c6a178c1876e
Shira Cohen <shira140@gmail.com>
2023-10-02 15:29:46 +0300

    db url for production update

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://shira:308417674@cluster0.ocluy2q.mongodb.net/?retryWrites=true&w=majority',
+
   dbName: 'toy_db'
 }
